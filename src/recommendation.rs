9b0553a2a1e593f090ca02af8d869d50bbfdc69e
DimitriTimoz <dimitri.timoz@orange.fr>
2023-09-24 01:14:34 +0200

    some fixes to compute estimated rating

--- src/recommendation.rs
+++ src/recommendation.rs
@@ -1,20 +1,20 @@
 use std::ops::Mul;
 
 use crate::prelude::*;
-use nalgebra_sparse::{coo::CooMatrix, CsrMatrix, CscMatrix, na::{RowVector, Similarity}};
+use nalgebra_sparse::{coo::CooMatrix, CsrMatrix, CscMatrix};
 
 
-pub fn compute_matrix(similar_users: Vec<(UID, HashMap<MID, u8>)>, me: HashMap<MID, u8>, n_movies: usize) -> CooMatrix<f32> {
+pub fn compute_matrix(similar_users_rates: Vec<(UID, HashMap<MID, u8>)>, me: HashMap<MID, u8>, n_movies: usize) -> CooMatrix<f32> {
     let unique_movies: HashSet<MID> = HashSet::from_iter(
-        similar_users
+        similar_users_rates
             .iter()
             .flat_map(|(_, movies)| movies.keys().cloned()),
     );
 
-    let mut matrix = CooMatrix::new(similar_users.len() + 1, unique_movies.len());
+    let mut matrix = CooMatrix::new(similar_users_rates.len() + 1, unique_movies.len());
 
     // Add the users
-    for (i, (user, movies)) in similar_users.iter().enumerate() {
+    for (i, (user, movies)) in similar_users_rates.iter().enumerate() {
         for (j, movie) in unique_movies.iter().enumerate() {
             if let Some(rating) = movies.get(movie) {
                 matrix.push(i, j, *rating as f32);
@@ -25,7 +25,7 @@ pub fn compute_matrix(similar_users: Vec<(UID, HashMap<MID, u8>)>, me: HashMap<M
     // Add me
     for (j, movie) in unique_movies.iter().enumerate() {
         if let Some(rating) = me.get(movie) {
-            matrix.push(similar_users.len(), j, *rating as f32);
+            matrix.push(similar_users_rates.len(), j, *rating as f32);
         }
     }
     matrix
@@ -55,7 +55,7 @@ pub async fn get_recommendations(
     matrix: &CsrMatrix<f32>,
     norms: &[f32],
     to_predict: usize,
-) -> Vec<(MID, u8)> {
+) -> Vec<(MID, f32)> {
     // Take a row from the matrix and convert it to a Matrix
     println!("computing the row");
     let row = matrix.row(to_predict);
@@ -77,25 +77,29 @@ pub async fn get_recommendations(
     const K: usize = 100;
     let mut top_k = [(0.0, 0); K];
     let mut n = 0;
+  
     for (i, mut row) in sim.row_iter_mut().enumerate() {
         for value in row.values_mut() {
-            if value != &0.0 {
-                *value /= norms[to_predict] * norms[i];
-            } else {
-                *value = -1.0;
-            }            
-
+            // Divide by the norms to get the cosine similarity 
+            if norms[i] == 0.0 {
+                continue;
+            }
+            *value /= norms[to_predict] * norms[i];    
+            
             if i == to_predict {
                 *value = -1.0;
+                continue;
             }
-            
+
+            // Get the top k of most similar users
             if n < K {
                 top_k[n] = (*value, i);
                 n += 1;
             } else {
                 let mut min = 0;
+           
                 for (j, (v, _)) in top_k.iter().enumerate() {
-                    if v < value {
+                    if v < &top_k[min].0 {
                         min = j;
                     }
                 }
@@ -103,13 +107,49 @@ pub async fn get_recommendations(
             }
         }
     }
-
     // Build the matrix with the top k similar users
     println!("building the matrix with the top k similar users");
-    // Get the movies that the user hasn't seen
-    println!("{:?}", top_k);
-   
+    let mut similar_users_rates = CooMatrix::new(K, matrix.ncols()); // The matrix with the top k similar users of the shape (K, n_movies)
+    let mut similarities_users = CooMatrix::new(1, K); // The matrix with the similarities between the user to recommand and each other user of the shape (1, K)
+    // Put the data in the matrices
+    for (i, (similarity, user)) in top_k.iter().enumerate() {
+        similarities_users.push(0, i, *similarity);
+        for (j, v) in matrix.row(*user).values().iter().enumerate() {
+            similar_users_rates.push(i, j, *v);
+        }
+    }
 
-    Vec::new()
+    // Convert the matrices to CscMatrix
+    let similar_users_rates = CscMatrix::from(&similar_users_rates);
+    let similarities_users = CscMatrix::from(&similarities_users);
+    
+    // Compute the sum of the similarities with the following part of the fomula: sum(similarities_users * similar_users_rates)
+    let mut estimation = similarities_users.clone().mul(&(similar_users_rates.clone())); // Of the shape (1, n_movies)
+    for (i, mut col) in estimation.col_iter_mut().enumerate() {
+        // Get the column i of the similar_users_rates matrix and do the sum
+        let mut s = 0.0;
+        for v in similar_users_rates.get_col(i).unwrap().values().iter() {
+            s += v.abs();
+        }
+
+        for value in col.values_mut() {
+            *value /= s;
+        }
+    }
+    
+    // Get the movies that the user has not seen yet and sort them by the predicted rating
+    println!("getting the movies that the user has not seen yet and sort them by the predicted rating");
+    let mut movies = Vec::new();
+    for (i, value) in estimation.col_iter().enumerate() {
+        if let Some(v) = value.get_entry(0)  {
+            let v = v.into_value();
+            if v > 0.0 {
+                movies.push((i, v));
+            }
+        }
+    }
+    movies.sort_by(|(_, v1), (_, v2)| v2.partial_cmp(v1).unwrap());
+    println!("{:?}", movies);
+    movies
 }
 
