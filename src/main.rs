7690773df13ae2f59f5ee6b4138c960d7a3d0764
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-05-08 01:32:12 +0200

    Compare results

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,17 @@ async fn main() {
     println!("computing norms");
     let norms = compute_norms(&matrix);
     
-
     for (i, user) in users.iter().enumerate() {
         let reco = recommendation::get_recommendations(&matrix, &norms, i).await;
+
+        // Check if the recommendations are valid
+        let (user_id, user_ratings) = user;
+        for (movie, rating) in reco {
+            let rating = rating * 5.0;
+            let real_rating = user_ratings.get(&movie).unwrap_or(&0.0);
+            if real_rating > &0.0 {
+                println!("Movie {} was rated {} by user {} and the recommendation is {}, difference: {}", movie, real_rating, user_id, rating, real_rating - rating);
+            } 
+        }
     }
-    
 }
